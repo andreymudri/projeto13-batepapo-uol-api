12d4fbdd46b5dc963c17f063dc277c5d141e729a
Andrey Mudri <andreybeckert@gmail.com>
2023-04-14 00:16:40 -0300

    get post messages

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import  {MongoClient}  from 'mongodb';
+import { MongoClient } from "mongodb";
 import express from "express";
 import cors from "cors";
-import joi from 'joi';
-import dotenv from 'dotenv';
-import dayjs from 'dayjs';
+import joi from "joi";
+import dotenv from "dotenv";
+import dayjs from "dayjs";
 
 const app = express();
 app.use(express.json());
@@ -13,64 +13,117 @@ dotenv.config();
 
 let db;
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
-mongoClient.connect()
-    .then(() => db = mongoClient.db())
-    .catch((err) => console.log(err.message));
-/* */
+mongoClient
+  .connect()
+  .then(() => (db = mongoClient.db()))
+  .catch((err) => console.log(err.message));
 
-/* */
-app.post('/participants', async (req, res) => {
-    const participantSchema = joi.object({
-        name: joi.string().required()
+app.post("/participants", async (req, res) => {
+  const participantSchema = joi.object({
+    name: joi.string().required(),
+  });
+  try {
+    const { error } = participantSchema.validate(req.body);
+    if (error) {
+      return res.status(422).json({ error: error.details[0].message });
+    }
+    const { name } = req.body;
+    const existingParticipant = await db
+      .collection("participants")
+      .findOne({ name });
+    if (existingParticipant) {
+      return res.status(409).json({ error: "Participant name already in use" });
+    }
+    const participant = { name, lastStatus: Date.now() };
+    await db.collection("participants").insertOne(participant);
+    const time = dayjs().format("HH:mm:ss");
+    const message = {
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time,
+    };
+    await db.collection("messages").insertOne(message);
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/participants", async (req, res) => {
+  try {
+    const participants = await db.collection("participants").find().toArray();
+
+    return res.json(participants);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  try {
+    const { to, text, type } = req.body;
+    const from = req.headers.user;
+
+    const schema = joi.object({
+      to: joi.string().required(),
+      text: joi.string().required(),
+      type: joi.string().valid("message", "private_message").required()
     });
-    try {
-        const { error } = participantSchema.validate(req.body);
-        if (error) {
-            return res.status(422).json({ error: error.details[0].message });
-        }
-
-        const { name } = req.body;
-
-
-        const existingParticipant = await db.collection('participants').findOne({ name });
-        if (existingParticipant) {
-            return res.status(409).json({ error: 'Participant name already in use' });
-        }
-
-        const participant = { name, lastStatus: Date.now() };
-        await db.collection('participants').insertOne(participant);
-
-
-        const time = dayjs().format('HH:mm:ss');
-        const message = { from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time };
-        await db.collection('messages').insertOne(message);
-
-        return res.sendStatus(201);
-
-    } catch (error) {
-        console.log(error.message);
-        return res.status(500).json({ error: 'Internal Server Error' });
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(422).json({ error: error.details[0].message });
     }
+
+    const participant = await db
+      .collection("participants")
+      .findOne({ name: from });
+    if (!participant) {
+      return res.status(422).json({ error: "Invalid sender (from)" });
+    }
+
+    const time = dayjs().format("HH:mm:ss");
+    const message = { from, to, text, type, time };
+    await db.collection("messages").insertOne(message);
+
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
-/* */
-app.get('/participants', async (req, res) => {
-    try {
+app.get("/messages", async (req, res) => {
+  try {
+    const user = req.headers.user;
+    const limit = parseInt(req.query.limit);
 
-        const participants = await db.collection('participants').find().toArray();
-
-        return res.json(participants);
-
-    } catch (error) {
-        console.log(error.message);
-        return res.status(500).json({ error: 'Internal Server Error' });
+    if (isNaN(limit) || limit <= 0) {
+      return res.status(422).send("Invalid limit parameter");
     }
+
+    const messages = db.collection("messages");
+    const query = {
+      $or: [
+        { to: "Todos" },
+        { from: user },
+        { to: user },
+        { type: "public" }
+      ]
+    };
+    const result = await messages
+      .find(query)
+      .limit(limit)
+      .toArray();
+
+    return res.json(result);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
 });
-/* */
-
-
-
-
-/* */
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server online port ${PORT}.`));
